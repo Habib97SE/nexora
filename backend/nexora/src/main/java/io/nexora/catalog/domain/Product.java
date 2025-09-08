cdef4fc1f441906127d936aafb722fe85cf5656c
Habib Hezarehee <hezarehee.h@gmail.com>
2025-09-08 23:15:28 +0200

    feat (backend-catalog): add fields to Product entity

--- backend/nexora/src/main/java/io/nexora/catalog/domain/Product.java
+++ backend/nexora/src/main/java/io/nexora/catalog/domain/Product.java
@@ -1,4 +1,36 @@
 package io.nexora.catalog.domain;
 
+import io.nexora.shared.valueobject.Money;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.boot.jaxb.mapping.spi.db.JaxbColumnDefinable;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotNull
+    @Min(3)
+    private String name;
+
+    private String description;
+    @Embedded
+    private Money price;
+
+    @Column(columnDefinition = "bigint default 0")
+    private int stockQuantity;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Category category;
+
+
 }
---
+++ backend/nexora/src/main/java/io/nexora/shared/valueobject/Money.java
@@ -0,0 +1,4 @@
+package io.nexora.shared.valueobject;
+
+public record Money() {
+}
