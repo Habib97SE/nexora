f0bfe302b1a360d4d4b71d0db4da6aec84730ea8
Habib Hezarehee <hezarehee.h@gmail.com>
2025-09-13 11:58:12 +0200

    feat (backend-user): Enable JPA repositories for user infrastructure
    Added 'io.nexora.user.infrastructure' to the list of base packages for JPA repositories in NexoraApplication. This allows Spring Data JPA to detect and configure repositories in the user infrastructure module.

--- backend/nexora/src/main/java/io/nexora/NexoraApplication.java
+++ backend/nexora/src/main/java/io/nexora/NexoraApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "io.nexora.catalog.infrastructure")
+@EnableJpaRepositories(basePackages = {"io.nexora.catalog.infrastructure", "io.nexora.user.infrastructure"})
 public class NexoraApplication {
 
     public static void main(String[] args) {
